5feb85d058962ed562c0430831bbc6c88843707d
elvinsomon <elvinsomon2001@gmail.com>
2020-06-10 15:29:17 -0400

    Esquema hecho, estilos + JS.

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//alert('Hola, me llamo Elvin');
+
+document.querySelectorAll('.contenedor-modal .overlay').forEach((el) => {
+	el.addEventListener('click', function (ev) {
+		ev.stopPropagation();
+		this.parentNode.classList.add('active');
+	});
+});
+
+document.querySelectorAll('.contenedor-modal ').forEach((el) => {
+	el.addEventListener('click', function (ev) {
+		this.classList.remove('active');
+	});
+});
